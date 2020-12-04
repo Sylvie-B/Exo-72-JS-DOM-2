bbcd4f08dfe9ef63432646f162b40987e26812bc
Sylvie-B <unsolo@hotmail.fr>
2020-12-04 10:35:21 +0100

    exo 72 hello world

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,10 +1,9 @@
+let mesDivs = document.getElementsByClassName("name-tag");
 
 
-
-
-
-
-
-
-
+for(let i = 0; i < mesDivs.length; i++){
+    let newP = document.createElement("p");
+    newP.innerHTML = "HELLO WORLD";
+    mesDivs[i].appendChild(newP);
+}
 
